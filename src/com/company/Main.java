dbe1f8d79286bddf1648953855e7507002872178
mac <shattyk7979@mail.ru>
2022-02-03 21:17:10 +0600

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello gitHub !!!");
+        System.out.println("Hello new lesson !!!");
+        System.out.println("understand !!!");
+        System.out.println(" gitHub is very good !!!");
     }
 }
